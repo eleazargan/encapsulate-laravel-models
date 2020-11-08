ae927f187d61cb1453b0f1c153d58ffdf384767c
eleazargan <eleazar11egkh@gmail.com>
2020-11-08 22:36:07 +0800

    Added user repository

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,19 +1,29 @@
 <?php
 
+namespace App\Repositories;
+
+use App\Models\User;
+
 class UserRepository
 {
   public function store($data)
   {
+    $user = User::create($data);
 
+    return $user;
   }
 
   public function getById($id)
   {
+    $user = User::find($id);
 
+    return $user;
   }
 
   public function getAll()
   {
+    $users = User::query()->paginate(15);
 
+    return $user;
   }
 }
